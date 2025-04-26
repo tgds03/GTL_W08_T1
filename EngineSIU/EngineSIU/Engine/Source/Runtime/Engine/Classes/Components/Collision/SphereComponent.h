6f3ea2db6ad718438b00f9231e0dc0ac221ab3b3
Jay <seojoongwon2020@gmail.com>
2025-04-26 20:27:56 +0900

    Add: Create UShapeComponent and USphereComponent classes

---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Components/Collision/SphereComponent.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "Components/ShapeComponent.h"
+class USphereComponent : public UShapeComponent
+{
+    float SphereRadius;
+};
+
---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Components/ShapeComponent.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "PrimitiveComponent.h"
+class UShapeComponent : public UPrimitiveComponent
+{
+    FColor ShapeColor;
+    bool bDrawOnlyIfSelected;
+};
+
